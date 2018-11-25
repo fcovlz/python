6dc46de3cda45a6d7529a5874b9372216b249802
fcovlz <francisco.vlz.mtz@gmail.com>
2018-11-25 14:45:28 -0600

    Sorting

--- algorithms/search.py
+++ algorithms/search.py
@@ -1,80 +1,86 @@
 
+class Search():
 
-def busqueda_seq(lista, llave):
-    if llave is None or llave < 0:
+    def __init__(self, data, key, verbose=True):
+        self.data = data
+        self.key = key
+        self.verbose = verbose
+
+    def busqueda_seq(self):
+
+        for element in self.data:
+            if self.verbose:
+                print "DEBUG: ", element
+            if element == self.key:
+                return True
+
+        print "Item not found"
+        return False
+
+    def busqueda_seq_idx(lista, mul, llave):
+
+        if mul is None or llave is None:
+            print "enter a valid mul or llave"
+            return -1
+        res = len(lista) % mul
+        if res < .5:
+            div = (len(lista)/mul) - res
+        else:
+            div = (len(lista)/mul) + (res > 0)
+        for n in range(mul):
+            if n == 0:
+                index = lista[0:div]
+            if n == mul:
+                index = lista[(n-1)*div:]
+            else:
+                index = lista[n * div : ( 1 + n ) * div]
+            # print "DEBUG: ", n
+            # print index
+            if max(index) >= llave:
+                return busqueda_seq_fast(index, llave)
+        print "Item not found"
         return -1
-    for n in lista:
-        if n == llave:
+
+
+    def busqueda_seq_fast(lista, llave):
+        if llave is None or llave < 0:
+            return -1
+
+        mid = (max(lista) - min(lista))/2
+        mid += min(lista)
+        mid = int(round(mid))
+        # print "DEBUG: mid = ",mid
+
+        if max(lista) == llave or min(lista) == llave or mid == llave:
             return 1
-        print "DEBUG: ",n
-    print "Item not found"
-    return -1
 
-
-def busqueda_seq_idx(lista, mul, llave):
-
-    if mul is None or llave is None:
-        print "enter a valid mul or llave"
+        elif mid > llave:
+            midd = mid - (len(lista) / 4)
+            # print "DEBUG midd = ", midd
+            if llave >= midd:
+                for n in range(1, len(lista)/2):
+                    if (lista[(len(lista)/2) - n] == llave):
+                        return 1
+                    # print "DEBUG: ",n, lista[(len(lista)/2)-n]
+            else:
+                for n in range(0, len(lista)/2):
+                    if lista[n] == llave:
+                        return 1
+                    # print "DEBUG: ",n, (lista[n])
+        else:
+            midd = (len(lista) / 4) + mid
+            # print "DEBUG midd = ", midd
+            if llave>=midd:
+                for n in range(1, len(lista)/2):
+                    if lista[len(lista)-n] == llave:
+                        return 1
+                    elif midd == llave:
+                        return 1
+                    # print "DEBUG: ",n, lista[len(lista)-n]
+            else:
+                for n in range(0,len(lista)/2):
+                    if lista[(len(lista)/2) + n] == llave:
+                        # print "DEBUG: ",n, (lista[(len(lista)/2)+n])
+                        return 1
+           print "Item not found"
         return -1
-    res = len(lista) % mul
-    if res < .5:
-        div = (len(lista)/mul) - res
-    else:
-        div = (len(lista)/mul) + (res > 0)
-    for n in range(mul):
-        if n == 0:
-            index = lista[0:div]
-        if n == mul:
-            index = lista[(n-1)*div:]
-        else:
-            index = lista[n * div : ( 1 + n ) * div]
-        # print "DEBUG: ", n
-        # print index
-        if max(index) >= llave:
-            return busqueda_seq_fast(index, llave)
-    print "Item not found"
-    return -1
-
-
-def busqueda_seq_fast(lista, llave):
-    if llave is None or llave < 0:
-        return -1
-
-    mid = (max(lista) - min(lista))/2
-    mid += min(lista)
-    mid = int(round(mid))
-    # print "DEBUG: mid = ",mid
-
-    if max(lista) == llave or min(lista) == llave or mid == llave:
-        return 1
-
-    elif mid > llave:
-        midd = mid - (len(lista) / 4)
-        # print "DEBUG midd = ", midd
-        if llave >= midd:
-            for n in range(1, len(lista)/2):
-                if (lista[(len(lista)/2) - n] == llave):
-                    return 1
-                # print "DEBUG: ",n, lista[(len(lista)/2)-n] 
-        else:
-            for n in range(0, len(lista)/2):
-                if lista[n] == llave:
-                    return 1
-                # print "DEBUG: ",n, (lista[n])
-    else:
-        midd = (len(lista) / 4) + mid
-        # print "DEBUG midd = ", midd
-        if llave>=midd:
-            for n in range(1, len(lista)/2):
-                if lista[len(lista)-n] == llave:
-                    return 1
-                elif midd == llave:
-                    return 1
-                # print "DEBUG: ",n, lista[len(lista)-n] 
-        else:
-            for n in range(0,len(lista)/2):
-                if lista[(len(lista)/2) + n] == llave:
-                    # print "DEBUG: ",n, (lista[(len(lista)/2)+n])
-                    return 1
-       print "Item not found"
-    return -1
--- algorithms/sorting.py
+++ algorithms/sorting.py
@@ -1,44 +1,75 @@
 
-
 class Sorting(object):
 
-    def __init__(self):
-        self.lista = [1, -6, 2, 7, 0, 4, 5, 6, 8, -4, -2, -1]
+    def __init__(self, lista, verbose=True):
+        self.lista = lista
+        self.verbose = verbose
 
     def ord_inserccion(self):
-        for n in range(len(self.lista) - 1):
-            if self.lista[n] > self.lista[n + 1]:
-                v = self.lista[n + 1]
-                j = n+1
-                while j > 0 and v < self.lista[j - 1]:
-                    print self.lista
-                    self.lista[j] = self.lista[j - 1]
-                    j -= 1
-                self.lista[j] = v
-            print "DEBUG: ", self.lista
+
+        for index0 in range(len(self.lista) - 1):
+            current_value = self.lista[index0]
+            next_value = self.lista[index0 + 1]
+
+            if current_value > next_value:
+
+                tmp_index0 = index0 + 1
+
+                while tmp_index0 > 0 and \
+                        next_value < self.lista[tmp_index0 - 1]:
+
+                    self.lista[tmp_index0] = self.lista[tmp_index0 - 1]
+                    tmp_index0 -= 1
+
+                self.lista[tmp_index0] = next_value
+
+            if self.verbose:
+                print "DEBUG: ", self.lista
+
+        return self.lista
 
     def ord_seleccion(self):
-        for i in range(0,len(self.lista) - 1):
-            min = i
-            for j in range(i + 1, len(self.lista)):
-                if self.lista[min] > self.lista[j]:
-                    min = j
-            aux = self.lista[min]
-            self.lista[min] = self.lista[i]
-            self.lista[i] = aux
-            print "DEBUG: ", self.lista
 
-    def ord_burbuja(self):
-        for n in range(0, len(self.lista) - 1):
-            j = n + 1
-            for i in range(len(self.lista) - n):
-                if self.lista[n] > self.lista[j]:
-                    temp = self.lista[n]
-                    self.lista[n] = self.lista[j]
-                    self.lista[j] = temp
-                j += 1
-                print "DEBUG0: ", self.lista
-            print "DEBUG1: ", self.lista
+        for index0 in range(len(self.lista) - 1):
+            minimun_value = index0
+
+            for index1 in range(index0, len(self.lista)):
+                if self.lista[minimun_value] > self.lista[index1]:
+                    minimun_value = index1
+
+            self.lista[minimun_value], self.lista[index0] = \
+                self.lista[index0], self.lista[minimun_value]
+
+            if self.verbose:
+                print "DEBUG: ", self.lista
+
+        return self.lista
+
+    def ord_burbuja0(self):
+        for index0 in range(len(self.lista)-1):
+            for index1 in range(len(self.lista)-1):
+                if self.lista[index1] > self.lista[index1 + 1]:
+                    self.lista[index1], self.lista[index1+1] = \
+                        self.lista[index1+1], self.lista[index1]
+
+                if self.verbose:
+                    print "DEBUG: ", self.lista
+
+        return self.lista
+
+    def ord_seleccion_pythonic(self):
+        new_list = []
+        for index in range(len(self.lista)):
+
+            minimum_value = min(self.lista)
+            new_list.append(minimum_value)
+            self.lista.remove(minimum_value)
+
+            if self.verbose:
+                print "DEBUG: ", self.lista
+
+        self.lista = new_list
+        return self.lista
 
     def ord_mezcla(self, list):
         lista = list
@@ -73,33 +104,140 @@ class Sorting(object):
                 k = k + 1
             print "DEBUG: ", lista
 
+    def ord_original(self):
+        index = 0
+
+        while index < len(self.lista) - 1:
+
+            if self.lista[index] > self.lista[index + 1]:
+
+                self.lista[index], self.lista[index + 1] = \
+                    self.lista[index + 1], self.lista[index]
+
+                if index:
+                    index -= 1
+
+            else:
+                index += 1
+
+            print "DEBUG: ", self.lista
+
+        return self.lista
+
     def ord_rapido(self, list):
-        lista = list
+
         equal = []
         greater = []
         less = []
-        if len(lista) < 1:
-            return lista
-        else: 
-            pivot = lista[0]
-            for x in lista:
-                if x < pivot:
-                    less.append(x)
-                if x == pivot:
-                    equal.append(x)
-                if x > pivot:
-                    greater.append(x)
-            return  self.ord_rapido(less) + equal + self.ord_rapido(greater)
 
-    def ord_original(self):
-        n = 0
-        while n < len(self.lista) - 1:
-            if self.lista[n] > self.lista[n + 1]:
-                tmp = self.lista[n]
-                self.lista[n] = self.lista[n + 1]
-                self.lista[n + 1] = tmp
-                if n != 0:
-                    n -= 1
+        if len(list) < 1:
+            return list
+
+        else: 
+            reference = list[0]
+
+            for element in list:
+                if element < reference:
+                    less.append(element)
+                if element == reference:
+                    equal.append(element)
+                if element > reference:
+                    greater.append(element)
+
+                if self.verbose:
+                    print "###########################################"
+                    print "list :", list
+                    print "reference :", reference
+                    print "less than reference :", less
+                    print "equal to reference :", equal
+                    print "higher reference :", greater
+
+            self.list = self.ord_rapido(less) + equal + self.ord_rapido(greater)
+
+            return self.list
+
+    def quick_sort(self, list):
+        if len(list) <= 1:
+            return list
+        else:
+            reference = list[0]
+
+            if self.verbose:
+                print "###########################################"
+                print "list :", list
+                print "reference :", reference
+                print "lower than elem 0 :", [e for e in list if e < reference]
+                print "reference :", reference
+                print "higher elemen 0", [e for e in list if e > reference]
+
+            self.list = self.quick_sort([e for e in list if e < reference]) + \
+                [reference] + \
+                self.quick_sort([e for e in list if e > reference])
+
+            return self.list
+
+    def merge_sort(self, lista):
+        if len(lista) <= 1:
+            return lista
+
+        # Find the middle point and devide it
+        middle = len(lista) // 2
+        left_list = lista[:middle]
+        right_list = lista[middle:]
+
+        left_list = self.merge_sort(left_list)
+        right_list = self.merge_sort(right_list)
+
+        return self._merge(left_list, right_list)
+
+    def _merge(self, left_half, right_half):
+
+        result = []
+
+        while len(left_half) != 0 and len(right_half) != 0:
+
+            if left_half[0] < right_half[0]:
+                result.append(left_half[0])
+                left_half.remove(left_half[0])
             else:
-                n += 1
-            print "DEBUG: ", self.lista
+                result.append(right_half[0])
+                right_half.remove(right_half[0])
+
+        if len(left_half):
+            result += left_half
+        else:
+            result += right_half
+
+        if self.verbose:
+            print "result :", result
+
+        return result
+
+
+if __name__ == "__main__":
+    list = [1, 3, -1, 4, -2, -3, 10, 2, 5, -4, 0]
+    sort = Sorting(list)
+
+    result = sort.ord_burbuja0()
+    print result
+
+    result = sort.ord_seleccion()
+    print result
+
+    result = sort.ord_seleccion_pythonic()
+    print result
+
+    result = sort.ord_inserccion()
+    print result
+
+    result = sort.ord_original()
+    print result
+
+    result = sort.ord_rapido(list)
+    print result
+
+    result = sort.quick_sort(list)
+    print result
+
+    result = sort.merge_sort(list)
+    print result
