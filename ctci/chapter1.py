c2ee927f6f3b2494a434402d3396f21685536bd2
Francisco Velazquez <36943211+fcovlz@users.noreply.github.com>
2019-02-21 19:19:09 -0600

    Update chapter1.py

--- ctci/chapter1.py
+++ ctci/chapter1.py
@@ -2,7 +2,8 @@
 Chapter 1: Cracking the code interview
 Author: Francisco Velazquez
 """
-import datetime
+# region Problem 1.1
+
 
 def verify_unique_characters_in_string_solution1(data):
     """
@@ -11,7 +12,7 @@ def verify_unique_characters_in_string_solution1(data):
     What if you can not use additional data structures?
 
     :param data: must be an string
-    :return: True if unique characters
+    :return: True if unique characters in string
     """
     if len(data) > 1:
         for char in data[1:]:
@@ -29,7 +30,7 @@ def verify_unique_characters_in_string_solution2(data):
     What if you can not use additional data structures?
 
     :param data: must be an string
-    :return: True if unique characters
+    :return: True if unique characters in string
     """
     for index in range(len(data)):
         for element in data[index + 1:]:
@@ -42,9 +43,11 @@ def verify_unique_characters_in_string_solution3(data):
     """
     Implement an algorithm to determine if a string has all unique characters.
     What if you can not use additional data structures?
+
+    :param data: must be an string
+    :return: True if unique characters in string
     """
     validate_input_values(data)
-
     new_array = []
     for char in data:
         if char in new_array:
@@ -52,6 +55,147 @@ def verify_unique_characters_in_string_solution3(data):
         else:
             new_array.append(char)
     return True
+
+
+def verify_unique_characters_in_string_solution4(data):
+    """
+    Implement an algorithm to determine if a string has all unique characters.
+    What if you can not use additional data structures?
+
+    :param data: must be an string
+    :return: True if unique characters in string
+    """
+    validate_input_values(data)
+    data = sorted(data)
+    new_array = []
+    for char in data:
+        if char in new_array:
+            return False
+        else:
+            new_array.append(char)
+    return True
+
+# endregion Problem 1.1
+# region Problem 1.2
+
+
+def reverse_string_solution1(data):
+    """
+    Write code to reverse a C-Style String. (C-String means that 'abcd' is
+    represented as five characters, including the null character.)
+
+    :return: reversed string
+    """
+    validate_input_values(data)
+    data_output = ""
+    for char in data:
+        data_output = char + data_output
+    return data_output
+
+
+def reverse_string_solution2(data):
+    """
+    Write code to reverse a C-Style String. (C-String means that 'abcd' is
+    represented as five characters, including the null character.)
+
+    :return: reversed string
+    """
+    validate_input_values(data)
+    data = list(data)
+    for index in range(len(data)/2):
+        first_char = data[index]
+        data[index] = data[-1 - index]
+        data[-1 - index] = first_char
+    data = ''.join(data)
+    return data
+
+# endregion Problem 1.2
+# region Problem 1.3
+
+
+def remove_duplicate_char_in_string_solution1(data):
+    """
+    Design an algorithm and write code to remove the duplicate characters in a string
+    without using any additional bu#er. NOTE: One or two additional variables are !ne.
+    An extra copy of the array is not.
+
+    :return: string without duplicates
+    """
+    validate_input_values(data)
+
+    data = list(data)
+    for index in range(len(data)):
+        for element in data[index + 1:]:
+            if data[index] == element:
+                data = data[:index] + data[index+1:]
+    data = ''.join(data)
+    return data
+
+
+def remove_duplicate_char_in_string_solution1(data):
+    """
+    Design an algorithm and write code to remove the duplicate characters in a string
+    without using any additional bu#er. NOTE: One or two additional variables are !ne.
+    An extra copy of the array is not.
+
+    :return: string without duplicates
+    """
+    validate_input_values(data)
+
+    data = list(data)
+    for index in range(len(data)):
+        for element in data[index + 1:]:
+            if data[index] == element:
+                data = data[:index] + data[index+1:]
+    data = ''.join(data)
+    return data
+
+# endregion Problem 1.3
+# region Problem 1.4
+
+
+def verify_if_two_strings_are_anagrams_solution1(data1, data2):
+    """
+    Write a method to decide if two strings are anagrams or not.
+
+    :param data1: String reference 1
+    :param data2: String reference 2
+    :return:
+    """
+    if sorted(data1) == sorted(data2):
+        return True
+    else:
+        return False
+
+
+def verify_if_two_strings_are_anagrams_solution2(data1, data2):
+    """
+    Write a method to decide if two strings are anagrams or not.
+
+    :param data1: String reference 1
+    :param data2: String reference 2
+    :return:
+    """
+    if len(data1) != len(data2):
+        return False
+    dict_data1 = convert_string_to_dict(data1)
+    dict_dat2 = convert_string_to_dict(data2)
+    if dict_data1 == dict_dat2:
+        return True
+    else:
+        return False
+
+
+def convert_string_to_dict(data):
+    skip_char = {}
+    for index in range(len(data)):
+        char = data[index]
+        if char in skip_char:
+            skip_char[char] = skip_char[char] + 1
+            continue
+        skip_char[char] = 1
+    return skip_char
+# endregion Problem 1.4
 
 
 def validate_input_values(data=None):
@@ -62,20 +206,12 @@ def validate_input_values(data=None):
 
 
 if __name__ == "__main__":
-    input = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    start_sol1 = datetime.datetime.now()
-    sol1 = verify_unique_characters_in_string_solution1(input)
-    import time
-    time.sleep(2)
-    start_sol2 = datetime.datetime.now()
-    sol2 = verify_unique_characters_in_string_solution2(input)
-    start_sol3 = datetime.datetime.now()
-    sol3 = verify_unique_characters_in_string_solution3(input)
-    end_sol3 = datetime.datetime.now()
-    print sol1, sol2, sol3
-    sol1_exec_time = start_sol1-start_sol2
-    sol2_exec_time = start_sol2-start_sol3
-    sol3_exec_time = start_sol3-end_sol3
-    print sol1_exec_time.seconds
-    print sol2_exec_time.microseconds
-    print sol3_exec_time.microseconds
+    input = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZa"
+    # print verify_unique_characters_in_string_solution1(input)
+    # print verify_unique_characters_in_string_solution2(input)
+    # print verify_unique_characters_in_string_solution3(input)
+    # print verify_unique_characters_in_string_solution4(input)
+    # print reverse_string_solution1(input)
+    # print reverse_string_solution2(input)
+    # print remove_duplicate_char_in_string_solution1(input)
+    print verify_if_two_strings_are_anagrams_solution2(input, input)
