12b4d98d70407e1c82e766774f8a5c7a8c9a9090
Francisco Velazquez <36943211+fcovlz@users.noreply.github.com>
2019-12-18 15:26:53 -0600

    Deleted

--- parser_yaml.py
+++
@@ -1,9 +0,0 @@
-
-import yaml
-
-with open("yamo.yaml", 'r') as stream:
-    try:
-        print(yaml.load(stream))
-        config = yaml.safe_load(stream)
-    except yaml.YAMLError as exc:
-        print "Error on YAML file"
